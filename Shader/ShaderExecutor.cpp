14cab20e71c5e47685f90193fbcd5dc5a7e80bce
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-03-08 15:31:25 +0100

    update with score

--- Shader/ShaderExecutor.cpp
+++ Shader/ShaderExecutor.cpp
@@ -7,7 +7,6 @@
 #include <ossia/editor/state/state_element.hpp>
 #include <ossia/editor/scenario/time_interval.hpp>
 #include <QOpenGLWindow>
-#include <ossia/dataflow/graph/graph.hpp>
 namespace Shader
 {
 class ProcessExecutor final :
